3c6de073948e53b0e7a0a28aba8ad8997a6db34c
André Ilhicas dos Santos <andre.ilhicas@fiercely.pt>
2017-05-04 20:21:02 +0100

    FIX factory.py

--- factory.py
+++ factory.py
@@ -2,6 +2,7 @@ from __future__ import generators
 from bs4 import BeautifulSoup as bs
 import requests
 import time
+import json
 
 class Worker:
     def __init__(self, url):
@@ -18,10 +19,9 @@ class Worker:
         '''        
         soup = self.get_dom(self.url)
         regions = soup.select("#navigation > ul:nth-of-type(1) li a")
-        print (regions)
         for region in regions:
             self.urls[region.text] = region['href']
-    
+        
     def get_dom(self, url):
         '''
         Get DOM text in parsed html tree after BeautifulSoup Object conversion
@@ -31,7 +31,7 @@ class Worker:
             If successful Object BeautifulSoup else None
         '''
         #Do not abuse on requests
-        #time.sleep(1)
+        time.sleep(1)
         response = requests.get(url, headers=self.headers)
         
         if response.status_code == 200:
@@ -47,71 +47,89 @@ class Worker:
         Returns:
             Dict ({'item_name':{'url':url, 'type':item_type}})
         '''
-        
+        print(self.urls)
         url = self.urls[region]
-        
         soup = self.get_dom(url)
         region_urls = dict()
         if not soup:
             return None
-        
+        limit = 2+limit
         css_selector = '#winesortlist > div.tblbdr > table > tbody > tr:nth-of-type(1)'
-        print(soup)
         headers = soup.select(css_selector)[0].text.split()
-        css_selector = "#winesortlist > div.tblbdr > table > tbody > tr > td > a"
+        css_selector = "#winesortlist > div.tblbdr > table > tbody > tr > td"
         cells = soup.select(css_selector)
-        for i in range(limit):
-            region_urls[cells[i].text] = cells[i]['href']
-
+        for i in range(2,limit,5):
+            _type = cells[i].find('span')['class'][-1]
+            _name = cells[i].find('a').text
+            _url = cells[i].find('a')['href']
+            region_urls[_name] = {'url':_url, 'type':_type}
+            
         return region_urls
 
 class Item(object):
-    def factory(type, material):
-        if type == 'white': return WhiteWine(material)
-        if type == 'red': return RedWine(material)
-        if type == 'beverage': return Beverage(material)
+    '''
+    Class responsible for the return of Items of different instance types according to item to "manufacture"
+    '''
+    def factory(type, material, name):
+        if type == 'btlwht': return WhiteWine(material, name)
+        if type == 'btlred': return RedWine(material, name)
+        if type == 'beverage': return Beverage(material, name)
         assert 0, 'Not Implemented' + type
     factory = staticmethod(factory)
 
 
 class WhiteWine(Item):
-    def __init__(self, dom):
+    def __init__(self, dom, name):
         self.dom = dom
+        self.name = name
+        self._type = "White Wine"
+
+    def assemble(self):
+        self.price = self.dom.select("#tab > div > div > div.col2resulttemp2.noprint > div > div:nth-of-type(2) > span.dtlbl.sidepanel-text")[0].find('b').text.strip()
 
     def get_json(self):
-        return ''
+        json = {"type":self._type, "price":self.price, "name": self.name}
+        return json
 
 class RedWine(Item):
-    def __init__(self, dom):
+    def __init__(self, dom, name):
         self.dom = dom
-    
+        self.name = name
+        self._type = "Red Wine"
+        
     def assemble(self):
-        pass
+        self.alcohol = self.dom.select('#tab > div > div > div.col2resulttemp2.noprint > div > div:nth-of-type(10) > div')[0].find('b').text
 
     def get_json(self):
-        return ''
+        json = {"type":self._type, "alcohol":self.alcohol, "name": self.name}
+        return json
 
 class Beverage(Item):
-    def __init__(self, dom):
+    '''This is mocked instance'''
+    def __init__(self, dom, name):
         self.dom = dom
-    
+        self.name = "Mocked Instance of Beverage"
+        self._type = "Beverage"
+
     def assemble(self):
-        pass
+        self.spirit = "Vodka"
 
     def get_json(self):
-        return ''
+        json = {"type":self._type, "spirit":self.spirit, "name":self.name}
+        return json
 
 def item_input_generator(region):
     '''Generating doms as it gets them'''
     rdr = Worker('http://www.wine-searcher.com/regions.lml')
-    urls = rdr.get_region_urls(region)
-    for item in region.keys():
-        yield rdr.get_dom(region[item]['url']), item, region[item]['type']
+    urls = rdr.get_region_urls(region, limit=30)
+    print(urls)
+    for item in urls.keys():
+        yield rdr.get_dom(urls[item]['url']), item, urls[item]['type']
 
-items = [Item.factory(dom) for dom in item_input_generator('Portugal')]
+items = [Item.factory(info[2],info[0], info[1]) for info in item_input_generator('France')]
 
 for item in items:
     item.assemble()
-    item.get_json()
+    print(json.dumps(item.get_json(),indent=4))
 
 
